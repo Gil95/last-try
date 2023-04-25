302f90455a759d83d0011d6cc5aa7cf4aadd4895
Gil95 <gilsongomesneto.neto@gmail.com>
2023-04-25 02:56:59 -0300

    updated project

---
+++ script/script.js
@@ -0,0 +1,11 @@
+function englishVersion(){
+    romanianVer.style.display="none"
+    language.style.display="none"
+    englishVer.style.display="block"
+}
+
+function romanianVersion(){
+    englishVer.style.display="none"
+    language.style.display="none"
+    romanianVer.style.display="block"
+}
