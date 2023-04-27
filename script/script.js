4b388891ec59d7574243dd68312eacc5bf2ecdd7
Gil95 <gilsongomesneto.neto@gmail.com>
2023-04-27 01:02:42 -0300

    updated project

--- script/script.js
+++ script/script.js
@@ -5,3 +5,15 @@ function englishVersion(){
 function romanianVersion(){
     window.open("index-romanesc.html", '_self')
 }
+
+function sorryEnglish(){
+    window.open("i'm-sorry.html", '_self')
+}
+
+function reasonsEnglish(){
+    window.open(".html", '_self')
+}
+
+function missYouEnglish(){
+    window.open("i'm-sorry.html", '_self')
+}
